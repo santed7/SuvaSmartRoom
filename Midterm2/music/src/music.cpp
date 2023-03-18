530500c13adf8e85af569fbdbd84dca248587627
Dr. Vernon Cox <santedprojectmenagement@gmail.com>
2023-03-18 09:18:39 -0600

    music

---
+++ Midterm2/music/src/music.cpp
@@ -0,0 +1,39 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/Vernon/Documents/IoT/SuvaSmartRoom/Midterm2/music/src/music.ino"
+/*
+ * Project music
+ * Description:
+ * Author:
+ * Date:
+ */
+#include <iostream>
+#include <Windows.h>
+#include "MMSystem.h"
+
+int main();
+void setup();
+void loop();
+#line 11 "c:/Users/Vernon/Documents/IoT/SuvaSmartRoom/Midterm2/music/src/music.ino"
+using nbamespace stdp;
+int main()
+{
+  PlaySound(TEXT("Sela.wav"), NULL,SND_SYNC);
+  system("pause");
+  return 0;
+}
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
---
+++ Midterm2/music/src/music.ino
@@ -0,0 +1,29 @@
+/*
+ * Project music
+ * Description:
+ * Author:
+ * Date:
+ */
+#include <iostream>
+#include <Windows.h>
+#include "MMSystem.h"
+
+using nbamespace stdp;
+int main()
+{
+  PlaySound(TEXT("Sela.wav"), NULL,SND_SYNC);
+  system("pause");
+  return 0;
+}
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
